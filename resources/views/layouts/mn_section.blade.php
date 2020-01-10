25c783168e227063dcb935e2a762d789e029ae2c
sachkovyug <sachkovyug@altarix.ru>
2020-01-11 02:40:09 +0400

    l-2 add favicons & fix small bug in home_contoller

--- resources/views/layouts/mn_section.blade.php
+++ resources/views/layouts/mn_section.blade.php
@@ -15,6 +15,15 @@
     <!-- Jquery UI CSS -->
     <link href="{{ asset('css/jquery-ui.min.css') }}" rel="stylesheet">
     <title>@yield('title')</title>
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#ffffff">
+    
 </head>
 <body class="h-full">
     <div class="body-page flex flex-col	h-full">
