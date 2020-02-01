8e326445a9d2fa04b2ede596a36a701d215f4e02
sachkovyug <sachkovyug@altarix.ru>
2020-02-01 15:21:27 +0400

    l-4 mail settings

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.mail.ru'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'probuzhdenie.info@mail.ru'),
+        'name' => env('MAIL_FROM_NAME', 'сайт церкви Пробуждение'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('probuzhdenie.info@mail.ru'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Cherch_001'),
 
     /*
     |--------------------------------------------------------------------------
