28aca10b23db45f422ed05c0ee3119452d078d6c
sachkovyug <sachkovyug@altarix.ru>
2020-02-07 19:16:39 +0400

    l-4 test

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
+use Mail;
 use Auth;
 
 class AdminController extends Controller
