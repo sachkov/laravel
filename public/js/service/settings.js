ca3dc771267c32797f2a2d5f17d4a49d154213a3
sachkovyug <sachkovyug@altarix.ru>
2020-03-30 16:27:16 +0400

    l-8

--- public/js/service/settings.js
+++ public/js/service/settings.js
@@ -1,20 +1,24 @@
 
+
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDVSwUzmTVJCqvVYts0Ur_GG4AR9WNAS7Q",
-    authDomain: "probuzhdenie-956a9.firebaseapp.com",
-    databaseURL: "https://probuzhdenie-956a9.firebaseio.com",
-    projectId: "probuzhdenie-956a9",
-    storageBucket: "probuzhdenie-956a9.appspot.com",
-    messagingSenderId: "152744733596",
-    appId: "1:152744733596:web:370e56b77f7bcff9a13659"
-};
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDVSwUzmTVJCqvVYts0Ur_GG4AR9WNAS7Q",
+//     authDomain: "probuzhdenie-956a9.firebaseapp.com",
+//     databaseURL: "https://probuzhdenie-956a9.firebaseio.com",
+//     projectId: "probuzhdenie-956a9",
+//     storageBucket: "probuzhdenie-956a9.appspot.com",
+//     messagingSenderId: "152744733596",
+//     appId: "1:152744733596:web:370e56b77f7bcff9a13659"
+// };
+
+console.log(GFB.firebaseConfig);
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(GFB.firebaseConfig);
 // Retrieve Firebase Messaging object.
 const messaging = firebase.messaging();
 // Add the public key generated from the console here.
-messaging.usePublicVapidKey("BLD-hYtKj86Z6nH4Zifo0_yBqnNYLmrvHoyTp8oJ83MMzliMD3tbBlKX7IBHI4t3c-AUOUE7yQ2H_Gs9Y7NILbU");
+messaging.usePublicVapidKey(GFB.PublicVapidKey);
 
 
 $( document ).ready(function(){
@@ -53,7 +57,7 @@ function getToken(){
         if (currentToken) {
             sendTokenToServer(currentToken);
             console.log("TOKEN");
-            console.log(currentToken);
+            console.log("new token "+currentToken);
             //updateUIForPushEnabled(currentToken);
         } else {
             // Show permission request.
@@ -64,11 +68,6 @@ function getToken(){
         console.log('An error occurred while retrieving token. ', err);
         sendTokenToServer("");
     });
-}
-
-function sendTokenToServer(token = ""){
-    console.log(token);
-    if(token == "") console.log("empty token");
 }
 
 function detectMob() {
@@ -83,8 +82,8 @@ function sendTokenToServer(token){
             token: token,
             site: site
         },
-        function(){
-            console.log("request was sent");
+        function(data){
+            console.log("token was "+data.status);
         },
         ()=>{}
     );
